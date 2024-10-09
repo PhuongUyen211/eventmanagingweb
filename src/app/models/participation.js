f7914f23b4d4e16fe84a44368e2ab8a2a629823b
PhuongUyen211 <2351170632@e.tlu.edu.vn>
2024-10-09 23:06:15 +0700

    Update participation.js

--- src/app/models/participation.js
+++ src/app/models/participation.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ParticipationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'event', required: true },
 });
 
 module.exports = mongoose.model('Participation', ParticipationSchema);
